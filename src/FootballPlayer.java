44f9d739a520094a792b3d5410b19b42b18336c5
Dilara Tekin <dilara-tekin2000@hotmail.com>
2022-12-12 14:05:13 +0300

    example

---
+++ src/FootballPlayer.java
@@ -0,0 +1,18 @@
+public class FootballPlayer {
+
+   int no;
+   String name;
+   int minutes;
+   boolean inPlay;
+   int numberOfGoals;
+
+   void play(int minutesToPlay){
+      minutes=minutes+minutesToPlay;
+   }
+   void score(){
+      numberOfGoals = numberOfGoals + 1;
+
+   }
+
+}
+
